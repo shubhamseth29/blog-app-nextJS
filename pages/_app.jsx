cd9ebca75a84bcf50ab66da92d4cc0620d299cda
Shubham Seth <Shubham.seth@centilytics.com>
2022-05-22 17:14:05 +0530

    SUBJECT: Imported Script

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import React from 'react';
 import { Layout } from '../components';
 import '../styles/globals.scss';
@@ -6,9 +7,9 @@ import '../styles/globals.scss';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9184389292080783"
-     crossorigin="anonymous"></script>
-      <Script strategy = 'lazyOn' src = {`'https://www.googletagmanager.com/gtag/js?id=G-J1ESKLW91W'`}>
+      <Script strategy = 'lazyOnload' src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9184389292080783"
+     crossorigin="anonymous"></Script>
+      <Script strategy = 'lazyOnload' src = {`'https://www.googletagmanager.com/gtag/js?id=G-J1ESKLW91W'`}>
       </Script>
       <Script strategy = 'lazyOnload'>
         {
