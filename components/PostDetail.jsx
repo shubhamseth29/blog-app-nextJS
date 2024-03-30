8ff40c114c49b4d435ef0c560056a9d6b19f34a4
seth-shubham <shubham.seth@spotdraft.com>
2024-03-30 18:13:04 +0530

    Add styling to code snippets

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,32 +1,68 @@
-import moment from 'moment';
-import React from 'react';
-
+import moment from 'moment'
+import React from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import {
+  qtcreatorDark,
+  rainbow,
+} from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 const PostDetail = ({ post }) => {
   const getContentFragment = (index, text, obj, type) => {
-    let modifiedText = text;
+    let modifiedText = text
+
+    console.log('console', obj)
 
     if (obj) {
       if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
+        modifiedText = <b key={index}>{text}</b>
       }
 
       if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
+        modifiedText = <em key={index}>{text}</em>
       }
 
       if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
+        modifiedText = (
+          <ul>
+            <b>{text} </b>
+          </ul>
+        )
+      }
+
+      if (obj.code) {
+        modifiedText = (
+          <SyntaxHighlighter key={index} language="javascript" style={rainbow}>
+            {text}
+          </SyntaxHighlighter>
+        )
       }
     }
 
     switch (type) {
       case 'heading-three':
-        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+        return (
+          <h3 key={index} className="mb-4 text-xl font-semibold">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        )
       case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        )
       case 'heading-four':
-        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+        return (
+          <h4 key={index} className="text-md mb-4 font-semibold">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        )
       case 'image':
         return (
           <img
@@ -36,48 +72,83 @@ const PostDetail = ({ post }) => {
             width={obj.width}
             src={obj.src}
           />
-        );
+        )
+
+      case 'code-block':
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item, i) => (
+              <SyntaxHighlighter
+                key={index}
+                language="javascript"
+                style={qtcreatorDark}
+              >
+                {text}
+              </SyntaxHighlighter>
+            ))}
+          </p>
+        )
       default:
-        return modifiedText;
+        return modifiedText
     }
-  };
+  }
 
   return (
     <>
-      <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 p-8">
-        <div className="relative overflow-hidden shadow-md mb-6">
-          <img src={post.featuredImage.url} alt="" className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
+      <div className="mb-8 rounded-lg bg-white bg-opacity-70 bg-clip-padding p-8 pb-12 shadow-lg backdrop-blur-sm backdrop-filter lg:p-8">
+        <div className="relative mb-6 overflow-hidden shadow-md">
+          <img
+            src={post.featuredImage.url}
+            alt=""
+            className="h-full w-full rounded-t-lg object-cover  object-top shadow-lg lg:rounded-lg"
+          />
         </div>
         <div className="px-4 lg:px-0">
-          <div className="flex items-center mb-8 w-full">
-            <div className="hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center">
+          <div className="mb-8 flex w-full items-center">
+            <div className="mr-8 hidden items-center items-center justify-center md:flex lg:mb-0 lg:w-auto">
               <img
                 alt={post.author.name}
                 height="30px"
                 width="30px"
-                className="align-middle rounded-full"
+                className="rounded-full align-middle"
                 src={post.author.photo.url}
               />
-              <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
+              <p className="ml-2 inline align-middle text-lg font-medium text-gray-700">
+                {post.author.name}
+              </p>
             </div>
             <div className="font-medium text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-2 inline h-6 w-6 text-pink-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
               </svg>
-              <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+              <span className="align-middle">
+                {moment(post.createdAt).format('MMM DD, YYYY')}
+              </span>
             </div>
           </div>
           <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
           {post.content.raw.children.map((typeObj, index) => {
-            const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+            const children = typeObj.children.map((item, itemindex) =>
+              getContentFragment(itemindex, item.text, item)
+            )
 
-            return getContentFragment(index, children, typeObj, typeObj.type);
+            return getContentFragment(index, children, typeObj, typeObj.type)
           })}
         </div>
       </div>
-
     </>
-  );
-};
+  )
+}
 
-export default PostDetail;
+export default PostDetail
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      use: options.defaultLoaders.babel,
+      include: /node_modules\/react-syntax-highlighter/,
+    })
+
+    return config
+  },
   reactStrictMode: true,
 }
